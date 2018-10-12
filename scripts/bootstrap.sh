e4191a35557c9e5f6199666ad02604ef957ccfff
Alok Kumar <rajalokan@gmail.com>
2018-10-12 08:00:47 +0530

    working tickstack setup

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,17 +14,30 @@ EOF
 ctx logger info "Installing and Configuring influxdb"
 sudo yum install -y influxdb
 sudo systemctl start influxdb
+PASSWORD='rajalokan'
+influx
+CREATE USER "rajalokan" WITH PASSWORD 'rajalokan' WITH ALL PRIVILEGES
+exit
+# Enable auth in influxdb
+sudo sed -i '/^\[http\]$/,/^\[/ s/^  # auth-enabled =.*/  auth-enabled = true/' /etc/influxdb/influxdb.conf
 
 ctx logger info "Installing and Configuring Kapacitor"
 sudo yum install -y kapacitor
 # Update influxdb details in kapacitor config
+sudo sed -i '/^\[\[influxdb\]\]$/,/^\[/ s/^  username =.*/  username = \"rajalokan\"/' /etc/kapacitor/kapacitor.conf
+sudo sed -i '/^\[\[influxdb\]\]$/,/^\[/ s/^  password =.*/  password = \"rajalokan\"/' /etc/kapacitor/kapacitor.conf
 sudo systemctl daemon-reload
 sudo systemctl start kapacitor
 
 ctx logger info "Installing and Configuring Chronograf"
 sudo yum install -y chronograf
+sudo systemctl start chronograf
 
 ctx logger info "Installing and Configuring Telegraf"
 sudo yum install -y telegraf
 # Update influx db config in telegraf
+sudo sed -i '/^\[\[outputs.influxdb\]\]$/,/^\[/ s/^  # urls =.*http.*/  urls = \[\"http:\/\/localhost:8086\"\]/' /etc/telegraf/telegraf.conf
+sudo sed -i '/^\[\[outputs.influxdb\]\]$/,/^\[/ s/^  # database =.*/  database = \"telegraf\"/' /etc/telegraf/telegraf.conf
+sudo sed -i '/^\[\[outputs.influxdb\]\]$/,/^\[/ s/^  # username =.*/  username = \"rajalokan\"/' /etc/telegraf/telegraf.conf
+sudo sed -i '/^\[\[outputs.influxdb\]\]$/,/^\[/ s/^  # password =.*/  password = \"rajalokan\"/' /etc/telegraf/telegraf.conf
 sudo systemctl start telegraf
